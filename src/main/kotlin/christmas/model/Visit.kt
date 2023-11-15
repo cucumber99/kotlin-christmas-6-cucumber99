c5d8c44e6bdb6fce8f6381106ced57b3a6c94714
cucumber99 <dlsguq1@gmail.com>
2023-11-15 11:39:41 +0900

    Refactor: 방문 날짜 모델 수정 및 요일 변환 추가

---
+++ src/main/kotlin/christmas/model/Visit.kt
@@ -0,0 +1,21 @@
+package christmas.model
+
+import christmas.util.Validator.validateDate
+
+class Visit (private val _date: Int){
+    private val _day: Int
+    val date: Int
+        get() = _date
+
+    val day: Int
+        get() = _day
+
+    init {
+        validateDate(_date)
+        _day = convertDate()
+    }
+
+    private fun convertDate(): Int {
+        return (_date % 7 + 3) % 7 + 1
+    }
+}

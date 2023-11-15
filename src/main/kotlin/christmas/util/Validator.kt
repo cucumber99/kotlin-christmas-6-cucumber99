0efa5696ba5e65f4f82547f17c1b629067dc0995
cucumber99 <dlsguq1@gmail.com>
2023-11-15 11:20:02 +0900

    feat: 메뉴 개수 유효성 검사 추가

--- src/main/kotlin/christmas/util/Validator.kt
+++ src/main/kotlin/christmas/util/Validator.kt
@@ -2,70 +2,82 @@ package christmas.util
 
 import christmas.model.Menu
 import christmas.util.Constants.COUNT_SEPARATOR
+import christmas.util.Constants.MAX_COUNT
 import christmas.util.Constants.MENU_SEPARATOR
 import christmas.util.Constants.PARTS_SIZE
 
 object Validator {
-    fun validateNull(input: String) {
+    fun validateDateNull(input: String) {
         require(input.trim().isNotEmpty()) {
-            ErrorMessage.INPUT_NULL.getMessage()
+            ErrorMessage.DATE_ERROR.getMessage()
+        }
+    }
+
+    fun validateMenuNull(input: String) {
+        require(input.trim().isNotEmpty()) {
+            ErrorMessage.MENU_ERROR.getMessage()
         }
     }
 
     fun validateDate(input: Int) {
         require(input in 1..31) {
-            ErrorMessage.DATE_RANGE.getMessage()
+            ErrorMessage.DATE_ERROR.getMessage()
         }
     }
 
     fun validateInteger(input: String) {
         require(input.toIntOrNull() != null) {
-            ErrorMessage.INPUT_NUMBER.getMessage()
+            ErrorMessage.DATE_ERROR.getMessage()
         }
     }
 
-    fun validateCounts(input: Collection<Int?>) {
-        for (count in input) {
-            require(count != null) {
-                ErrorMessage.MENU_NUMBER.getMessage()
+    fun validateCounts(input: Map<String, String>) {
+        input.values.forEach { countStr ->
+            val count = countStr.toIntOrNull()
+            require(count != null && count > 0) {
+                ErrorMessage.MENU_ERROR.getMessage()
             }
         }
     }
 
     fun validateTotalCount(input: Collection<Int>) {
-        for (count in input) {
-            require(count <= 20) {
-                ErrorMessage.MENU_COUNT.getMessage()
-            }
+        val total = input.sum()
+        require(total <= MAX_COUNT) {
+            ErrorMessage.MENU_ERROR.getMessage()
         }
     }
 
-    fun validateMenuNames(menus: Map<String, Int?>) {
+    fun validateMenuNames(menus: Map<String, Int>) {
         menus.keys.forEach { menuName ->
             require(Menu.entries.any { it.menuName == menuName }) {
-                ErrorMessage.MENU_NAME.getMessage()
+                ErrorMessage.MENU_ERROR.getMessage()
             }
         }
     }
 
-    fun validateDuplicate(input: String, menus: Map<String, Int?>) {
+    fun validateDuplicate(input: String, menus: Map<String, Int>) {
         val menuCount = input.split(MENU_SEPARATOR).size
         require(menuCount == menus.size) {
-            ErrorMessage.MENU_DUPLICATE.getMessage()
+            ErrorMessage.MENU_ERROR.getMessage()
         }
     }
 
-    fun validateBeverage(menus: Map<String, Int?>) {
+    fun validateBeverage(menus: Map<String, Int>) {
         val drinks = setOf(Menu.COKE.menuName, Menu.WINE.menuName, Menu.CHAMPAGNE.menuName)
         require(menus.keys.any { it !in drinks }) {
-            ErrorMessage.MENU_BEVERAGE.getMessage()
+            ErrorMessage.MENU_ERROR.getMessage()
         }
     }
 
+    fun validateNoBlank(input: String) {
+        require(!input.contains(" ")) {
+            ErrorMessage.MENU_ERROR.getMessage()
+        }
+    }
     fun validateMenuSeparator(input: String) {
         input.split(MENU_SEPARATOR).forEach { item ->
             require(item.contains(COUNT_SEPARATOR)) {
-                ErrorMessage.MENU_FORMAT.getMessage()
+                ErrorMessage.MENU_ERROR.getMessage()
             }
         }
     }
@@ -74,7 +86,7 @@ object Validator {
         input.split(MENU_SEPARATOR).forEach { item ->
             val parts = item.split(COUNT_SEPARATOR)
             require(parts.size == PARTS_SIZE) {
-                ErrorMessage.MENU_FORMAT.getMessage()
+                ErrorMessage.MENU_ERROR.getMessage()
             }
         }
     }

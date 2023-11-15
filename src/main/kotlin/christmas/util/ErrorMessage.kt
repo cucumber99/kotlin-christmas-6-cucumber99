5f364f028a5b29e0d6a6e608bed0866658dd97a0
cucumber99 <dlsguq1@gmail.com>
2023-11-15 16:47:32 +0900

    refactor: 오류 메시지 개행 문자 추가

--- src/main/kotlin/christmas/util/ErrorMessage.kt
+++ src/main/kotlin/christmas/util/ErrorMessage.kt
@@ -1,8 +1,8 @@
 package christmas.util
 
 enum class ErrorMessage(private val message: String) {
-    MENU_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    MENU_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요.\n"),
+    DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요.\n");
 
     fun getMessage(): String = "[ERROR] $message"
 }

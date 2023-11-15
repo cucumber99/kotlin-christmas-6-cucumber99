b9d9fbdc34798ae7e1976945b212e82a65b59eee
cucumber99 <dlsguq1@gmail.com>
2023-11-15 15:26:31 +0900

    feat: 출력 메시지 상수 추가

--- src/main/kotlin/christmas/util/Constants.kt
+++ src/main/kotlin/christmas/util/Constants.kt
@@ -3,5 +3,51 @@ package christmas.util
 object Constants {
     const val MENU_SEPARATOR = ","
     const val COUNT_SEPARATOR = "-"
+    const val NUMBER_FORMAT = "%,d"
     const val PARTS_SIZE = 2
+
+    const val ZERO = 0
+    const val MAX_COUNT = 20
+
+    const val FIRST = 1
+    const val CHRISTMAS = 25
+
+    const val DAILY_DISCOUNT = 1000
+    const val SPECIAL_DISCOUNT = 1000
+    const val DAILY_DISCOUNT_ADD = 100
+    const val DISCOUNT_AMOUNT = 2023
+    const val PRESENTATION_AMOUNT = 120_000
+
+    const val SUNDAY = 1
+    const val MONDAY = 2
+    const val TUESDAY = 3
+    const val WEDNESDAY = 4
+    const val THURSDAY = 5
+    const val FRIDAY = 6
+    const val SATURDAY = 7
+
+    const val STAR_BADGE = "별"
+    const val TREE_BADGE = "트리"
+    const val SANTA_BADGE = "산타"
+
+    const val STAR_BOUNDARY = 5_000
+    const val TREE_BOUNDARY = 10_000
+    const val SANTA_BOUNDARY = 20_000
+
+    const val START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+    const val ORDER_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+    const val EVENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"
+    const val MENU_MESSAGE = "<주문 메뉴>"
+    const val PRICE_MESSAGE = "<할인 전 총주문 금액>\n%s원\n"
+    const val PRESENTATION_MENU_MESSAGE = "<증정 메뉴>\n샴페인 1개\n"
+    const val DAILY_MESSAGE = "크리스마스 디데이 할인: -%s원"
+    const val WEEKLY_MESSAGE = "평일 할인: -%s원"
+    const val WEEKEND_MESSAGE = "주말 할인: -%s원"
+    const val SPECIAL_MESSAGE = "특별 할인: -%s원"
+    const val PRESENTATION_MESSAGE = "증정 이벤트: -25,000원\n"
+    const val TOTAL_DISCOUNT_MESSAGE = "<총혜택 금액>\n-%s원\n"
+    const val PAYMENT_MESSAGE = "<할인 후 예상 결제 금액>\n%s원\n"
+    const val BADGE_MESSAGE = "<12월 이벤트 배지>"
+
+    const val NONE_MESSAGE = "없음\n"
 }

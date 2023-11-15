662b25ac9a10042818394452e60b898ba1a96dd3
cucumber99 <dlsguq1@gmail.com>
2023-11-15 11:20:56 +0900

    Refactor: 오류 메시지 수정

--- src/main/kotlin/christmas/util/ErrorMessage.kt
+++ src/main/kotlin/christmas/util/ErrorMessage.kt
@@ -1,15 +1,8 @@
 package christmas.util
 
 enum class ErrorMessage(private val message: String) {
-    MENU_NAME("는 존재하지 않는 메뉴입니다."),
-    MENU_COUNT("총 메뉴의 개수는 20개를 초과할 수 없습니다."),
-    MENU_NUMBER("메뉴 개수의 입력은 숫자여야 합니다."),
-    MENU_DUPLICATE("메뉴는 중복될 수 없습니다."),
-    MENU_FORMAT("입력의 형식이 올바르지 않습니다."),
-    MENU_BEVERAGE("음료수만 주문할 수 없습니다."),
-    DATE_RANGE("날짜는 1부터 31까지 입력할 수 있습니다."),
-    INPUT_NUMBER("입력은 숫자여야 합니다."),
-    INPUT_NULL("값이 존재하지 않습니다.");
+    MENU_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     fun getMessage(): String = "[ERROR] $message"
 }

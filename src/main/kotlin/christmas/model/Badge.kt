53e01a04660f1bf6d08f9c68c99ce526f97d99f4
cucumber99 <dlsguq1@gmail.com>
2023-11-15 14:41:26 +0900

    feat: 배지 모델 추가

---
+++ src/main/kotlin/christmas/model/Badge.kt
@@ -0,0 +1,28 @@
+package christmas.model
+
+import christmas.util.Constants.SANTA_BADGE
+import christmas.util.Constants.SANTA_BOUNDARY
+import christmas.util.Constants.STAR_BADGE
+import christmas.util.Constants.STAR_BOUNDARY
+import christmas.util.Constants.TREE_BADGE
+import christmas.util.Constants.TREE_BOUNDARY
+
+class Badge(private val discount: Int) {
+    private val _badge: String
+
+    val badge: String
+        get() = _badge
+
+    init {
+        _badge = getBadge()
+    }
+
+    private fun getBadge(): String {
+        return when {
+            discount >= SANTA_BOUNDARY -> SANTA_BADGE
+            discount >= TREE_BOUNDARY -> TREE_BADGE
+            discount >= STAR_BOUNDARY -> STAR_BADGE
+            else -> ""
+        }
+    }
+}
